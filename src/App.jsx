7dc3b54e0f1d912f440537edd08c952c2576cea8
MateoIr <mateoiriso@gmail.com>
2025-02-03 19:10:15 -0300

    sprint terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./page/home/Home";
+import Contact from "./page/Contact/Contact";
 
 function App() {
   return (
-    <>
-      <div>hola</div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
