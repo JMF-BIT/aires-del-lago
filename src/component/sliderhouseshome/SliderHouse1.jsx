eb769e9429cbd94b50db66d84a73ac0f55507e60
JuaniBanus <juanbanuss@gmail.com>
2025-02-06 00:07:44 -0300

    juanitoelpistolero

---
+++ src/component/sliderhouseshome/SliderHouse1.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { Box } from "@mui/material";
+
+const HousePhotoSlider = ({ images, initialIndex = 0 }) => {
+  const [currentIndex, setCurrentIndex] = useState(initialIndex);
+
+  const handleMouseEnter = () => {
+    if (images.length > 1) {
+      setCurrentIndex(1); // Cambia a la segunda imagen al hacer hover
+    }
+  };
+
+  const handleMouseLeave = () => {
+    setCurrentIndex(initialIndex); // Vuelve a la imagen inicial al quitar el hover
+  };
+
+  return (
+    <Box
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      sx={{
+        width: "100%",
+        height: "100%",
+        alignItems: "center",
+        backgroundImage: `url(${images[currentIndex]})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        transition: "all 0.5s ease",
+      }}
+    />
+  );
+};
+
+export default HousePhotoSlider;
--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,97 +1,171 @@
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import "./Home.css";
-import { Grid, Box } from "@mui/material";
+import { Grid, Box, Button, Typography } from "@mui/material";
+import NavBar from "../../component/navbar/NavBar";
+import SliderHomeComponent from "../../component/sliderhome/SliderHomeComponent";
+import HousePhotoSlider from "../../component/sliderhouseshome/SliderHouse1";
+import { Card, CardContent, CardMedia } from "@mui/material";
+
+import imga1 from "../../imgs/casas/aires2.jpg";
+import imga2 from "../../imgs/casas/air2.jpg";
+import imga3 from "../../imgs/casas/aires3.jpg";
+import imga4 from "../../imgs/casas/air3.jpg";
+import imga5 from "../../imgs/casas/aires4.jpg";
+import imga6 from "../../imgs/casas/air4.jpg";
+import img8 from "../../imgs/home/cinta.jpg";
+
+const cardsData = [
+  {
+    id: 1,
+    images: [imga1, imga2],
+    title: "Aires 2",
+    description: "Capacidad para 6 personas.",
+  },
+  {
+    id: 2,
+    images: [imga3, imga4],
+    title: "Aires 3",
+    description: "Capacidad para 8 personas.",
+  },
+  {
+    id: 3,
+    images: [imga5, imga6],
+    title: "Aires 4",
+    description: "Capacidad para 6 personas.",
+  },
+];
 
 const Home = () => {
   return (
-    <Box className="mainBox">
-      <Box className="boxTextSup">
-        <Grid item xs={12} className="gridConteinerBoxSup">
-          <h1>Bienvenidos a</h1>
-          <p>La escapada que necesitás.</p>
-        </Grid>
-        <Grid item xs={12} className="gridConteinerBoxImgSup">
-          {/* imagen */}
-        </Grid>
-        <Grid item xs={12} className="gridConteinerBoxTextSup2">
-          <h2>Viví la experiencia KUNTUR</h2>
+    <>
+      <NavBar></NavBar>
+      <Box className="mainBox">
+        <Box className="boxSliderImage" position={"relative"}>
+          {/* link para slider de imagenes */}
+          <SliderHomeComponent />
+          <Box
+            className="boxSliderTextImage"
+            sx={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              color: "white", // Ajusta el color según tu imagen
+              textAlign: "center",
+              zIndex: 1,
+            }}
+          >
+            <Typography variant="h2" className="title">
+              AIRES DEL LAGO
+            </Typography>
+            <Typography variant="h4" className="subtitle">
+              Casas de Campo
+            </Typography>
+            <p>un lugar pensado para disfrutar.</p>
+          </Box>
+        </Box>
+
+        <Box className="boxContainerText">
+          <h1>¡BIENVENIDOS!</h1>
+          <h2>Conoce nuestras opciones para tu merecido descanso.</h2>
+          <img src={img8} alt="Decorative Ribbon" />
           <p>
-            Llevamos la experiencia de glamping al siguiente nivel, manteniendo
-            la simbiosis perfecta entre naturaleza y confort.
+            Aires del Lago es un lugar increíble ubicado frente al lago Los
+            Molinos, nuestro complejo de casas de alquiler ofrece comodidad y
+            elegancia. Disfruta de nuestra cancha de pádel y pileta al aire
+            libre. Ideal para familias y grupos de amigos. Relajación y
+            diversión en un entorno natural.
+            <p>
+              Veni y descubrí la experiencia perfecta, un lugar pensado para
+              vos!
+            </p>
           </p>
-        </Grid>
-      </Box>
-
-      <Box className="BoxConteinHouse">
-        <Grid container spacing={3} className="gridConteinerBoxHouse">
-          <Grid
-            item
-            xs={12}
-            md={6}
-            lg={4}
-            className="gridLeftConteinerBoxHouse"
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: "black",
+              color: "white",
+              mt: 2,
+              justifyContent: "center",
+            }} // margin top para separar el botón del texto
+            onClick={() => {
+              const element = document.getElementById("BoxConteinHouse");
+              if (element) {
+                element.scrollIntoView({ behavior: "smooth", block: "start" });
+              }
+            }}
           >
-            <img src="Ihome/INTI.jpg" alt="Domo Inti" />
-            <h3>Domo INTI</h3>
-            <p>
-              Concepto único para 2 personas que buscan una escapada romántica.
-            </p>
-            <a href="#">Ver más</a>
-          </Grid>
+            EXPLORA NUESTRAS CASAS
+          </Button>
+        </Box>
 
-          <Grid
-            item
-            xs={12}
-            md={6}
-            lg={4}
-            className="gridMediumConteinerBoxHouse"
-          >
-            <img src="Ihome/WASI.jpg" alt="Domo Wasi" />
-            <h3>Domo WASI</h3>
-            <p>
-              Diseñado para quienes desean una experiencia inolvidable en
-              familia o con amigos.
-            </p>
-            <a href="#">Ver más</a>
+        <Box id="BoxConteinHouse" className="boxConteinHouse">
+          <Grid container spacing={2}>
+            {cardsData.map((card) => (
+              <Grid item xs={12} sm={6} md={4} key={card.id}>
+                <Card
+                  sx={{
+                    boxShadow: 3,
+                    backgroundColor: "white",
+                    borderRadius: "10px",
+                    paddingBottom: "30px", // Espacio extra en la parte inferior
+                    border: "10px solid white", // Borde grueso estilo Polaroid
+                    boxShadow: "5px 5px 15px rgba(0, 0, 0, 0.3)",
+                    width: "400px", // Más ancho
+                    height: "auto ", // Más alto
+                    mx: "auto", // Centrar la tarjeta
+                  }}
+                >
+                  <CardMedia sx={{ height: 300, position: "relative" }}>
+                    <HousePhotoSlider images={card.images} />
+                  </CardMedia>
+                  <CardContent>
+                    <Typography
+                      variant="h2"
+                      sx={{
+                        fontSize: "1.2rem",
+                      }}
+                    >
+                      {card.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {card.description}
+                    </Typography>
+                    <a href="#">Ver más</a>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
           </Grid>
+        </Box>
 
-          <Grid
-            item
-            xs={12}
-            md={12}
-            lg={4}
-            className="gridRightConteinerBoxHouse"
-          >
-            <img src="Ihome/WASI.jpg" alt="Domo Wasi" />
-            <h3>Domo Mojito</h3>
-            <p>
-              Diseñado para quienes desean una experiencia inolvidable en
-              familia o con amigos.
-            </p>
-            <a href="#">Ver más</a>
+        <Box className="boxConteinerFooter">
+          <Grid spacing={10} item xs={12} className="gridConteinerBoxFooter">
+            <footer className="footer">
+              <a
+                href="https://www.instagram.com/airesdellago_/"
+                target="_blanck"
+                rel="noopener noreferrer"
+                className="icon1"
+              >
+                <InstagramIcon style={{ fontSize: "80px" }} />
+              </a>
+              <a
+                href="https://wa.me/5493518171664"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="icon2"
+              >
+                <WhatsAppIcon style={{ fontSize: "80px" }} />
+              </a>
+              <p>WhatsApp: +54 9 3518171664</p>
+              <p>Instagram: @airesdellago_</p>
+            </footer>
           </Grid>
-        </Grid>
+        </Box>
       </Box>
-
-      <Box className="boxConteinerFooter">
-        <Grid item xs={12} className="gridConteinerBoxFooter">
-          <footer className="footer">
-            <a href=""></a>
-            <a
-              href="https://wa.me/5493518171664"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="icon"
-            >
-              <WhatsAppIcon />
-            </a>
-            <p>WhatsApp: +54 9 3518171664</p>
-            <p>Instagram: @airesdellago_</p>
-          </footer>
-        </Grid>
-      </Box>
-    </Box>
+    </>
   );
 };
 
