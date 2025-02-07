4d1e1113f4bccd33445b161c92284167ff8acfdf
MateoIr <mateoiriso@gmail.com>
2025-02-07 16:37:41 -0300

    franco se la come

--- src/page/Casas/Aires4.jsx
+++ src/page/Casas/Aires4.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import NavBar from "../../component/navbar/NavBar";
 import ContentSectionHouse from "../../component/ContentSectionHouse/ContentSectionHouse.jsx";
 import "../../component/ContentSectionHouse/ContentSectionHouse.css";
@@ -11,6 +11,7 @@ import aires43 from "../../imgs/aires4/aires4-3.jpg";
 import aires44 from "../../imgs/aires4/aires4-4.jpg";
 import aires45 from "../../imgs/aires4/aires4-5.jpg";
 import Galery from "../../component/galeri/Galery.jsx";
+import TarifaItem from "../../component/tarifaItem/TarifaItem.jsx";
 
 const Aires2 = () => {
   const images = [
@@ -102,7 +103,66 @@ const Aires2 = () => {
         imageUrl={img3}
         reverse={true} // La imagen se coloca a la izquierda
       />
-      <Galery imgs={images}></Galery>
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
+          paddingTop: "30px",
+        }}
+      >
+        <Grid container sx={{ maxWidth: "1200px" }}>
+          <Grid
+            xs={12}
+            md={6}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              paddingTop: "30px",
+            }}
+          >
+            <TarifaItem
+              texto="pileta incluida"
+              img={aires44}
+              noches="2 noches"
+              precio="USD$400"
+            />
+          </Grid>
+          <Grid
+            xs={12}
+            md={6}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              paddingTop: "30px",
+            }}
+          >
+            <TarifaItem
+              texto="prohibido menores"
+              img={aires45}
+              noches="3 noches"
+              precio="USD$400"
+            />
+          </Grid>
+        </Grid>
+      </Box>
+
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          paddingTop: "30px",
+        }}
+      >
+        <Box sx={{ maxWidth: "1200px", maxHeight: "900px" }}>
+          <Galery imgs={images}></Galery>
+        </Box>
+      </Box>
     </>
   );
 };
