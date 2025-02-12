4790bd3b869f04c9e21d38fe4a8cb55523de0bda
JuaniBanus <juanbanuss@gmail.com>
2025-02-12 14:16:27 -0300

    tarifas corregido y listo para mostrar

--- src/page/tarifas/Tarifas.jsx
+++ src/page/tarifas/Tarifas.jsx
@@ -19,17 +19,17 @@ const Tarifas = () => {
           backgroundImage: `url(${img2})`,
           fontSize: { xs: "10px", md: "20px", lg: "20px" },
         }}
-        className="boxImagePrincipalTop"
+        className="boxImagePrincipalTop1"
       >
-        <Box className="boxPrincipalTextTop">
-          <h1 className="imagePrincipalTitleText">Tarifas</h1>
-          <p className="imagePrincipalText">¡Gracias por visitarnos!</p>
+        <Box className="boxPrincipalTextTop1">
+          <h1 className="imagePrincipalTitleText1">Tarifas</h1>
+          <p className="imagePrincipalText1">¡Gracias por visitarnos!</p>
         </Box>
       </Box>
       <Box className="tarifaContainer">
         <Grid container className="tarifaBox">
           <Grid xs={12} sx={{ justifyContent: "center", display: "flex" }}>
-            <h1>TARIFAS</h1>
+            <h1>Tarifas por temporada</h1>
           </Grid>
           <Grid
             xs={12}
@@ -40,83 +40,81 @@ const Tarifas = () => {
               textAlign: "center",
             }}
           >
-            <h3>
-              temporada ALTA: entre EL 1 DE diciembre y 28 de febrero. fines de
-              semana largos y vacaciones de julio TEMPORADA BAJA: ENTRE EL 1 DE
-              MARZO AL 30 DE NOVIEMBRE. Las tarifas pueden ser modificadas sin
+            <h4>
+              Temporada ALTA: entre el 1 de Diciembre y 28 de Febrero, fines de
+              semana largos y vacaciones de julio. Temporada BAJA: entre el 1 de
+              Marzo al 30 de Noviembre. Las tarifas pueden ser modificadas sin
               previo aviso. Los precios están expresados en dólares
-              estadounidenses. El pago se realizará en DOLARES O pesos
+              estadounidenses. El pago se realizará en Dolares o Pesos
               argentinos de acuerdo a la cotización del dólar blue venta que
               informa el sitio web de ámbito financiero al momento de cada pago
               -{" "}
               <a href="https://www.ambito.com/contenidos/dolar-informal.html">
                 https://www.ambito.com/contenidos/dolar-informal.html
               </a>
-            </h3>
+            </h4>
           </Grid>
           <Grid xs={12} className="tarifasItemBox">
             <h2>Aires 2 y Aires 4</h2>
           </Grid>
-          <Grid xs={12} md={6} className="tarifasItemBox">
+          <Grid xs={12} md={6} lg={4} className="tarifasItemBox">
             <TarifaItem
               img={img2}
               precio={"$200 USD"}
-              noches={"minimo 2 noches"}
-              texto={"precio por noche para 6 personas temporada alta"}
+              noches={"Minimo 2 noches"}
+              texto={"Precio por noche para 6 personas temporada alta"}
             ></TarifaItem>
           </Grid>
-          <Grid xs={12} md={6} className="tarifasItemBox">
+          <Grid xs={12} md={6} lg={4} className="tarifasItemBox">
             <TarifaItem
               img={img3}
               precio={"$150 USD"}
-              noches={"minimo 2 noches"}
-              texto={"precio por noche para 6 personas temporada baja"}
+              noches={"Minimo 2 noches"}
+              texto={"Precio por noche para 6 personas temporada baja"}
             ></TarifaItem>
           </Grid>
           <Grid xs={12} className="tarifasItemBox">
             <h2>Aires 3</h2>
           </Grid>
-          <Grid xs={12} md={6} className="tarifasItemBox">
+          <Grid xs={12} md={6} lg={4} className="tarifasItemBox">
             <TarifaItem
               img={img4}
               precio={"$250 USD"}
-              noches={"minimo 2 noches"}
-              texto={"precio por noche para 8 personas temporada alta"}
+              noches={"Minimo 2 noches"}
+              texto={"Precio por noche para 8 personas temporada alta"}
             ></TarifaItem>
           </Grid>
-          <Grid xs={12} md={6} className="tarifasItemBox">
+          <Grid xs={12} md={6} lg={4} className="tarifasItemBox">
             <TarifaItem
               img={img5}
               precio={"$200 USD"}
-              noches={"minimo 2 noches"}
-              texto={"precio por noche para 8 personas temporada baja"}
+              noches={"Minimo 2 noches"}
+              texto={"Precio por noche para 8 personas temporada baja"}
             ></TarifaItem>
           </Grid>
           <Grid
             xs={12}
+            className="gridTextBot"
             sx={{
               justifyContent: "center",
-              display: "flex",
               px: "12px",
             }}
           >
-            <h3>
-              ACLARACIONES ​ <br />
-              <ul>
-                <li>
-                  Fines de semana largos se alquilan completos sin excepción.
-                </li>
-                <li> Las tarifas pueden ser modificadas sin previo aviso.</li>
-                <li>
-                  Para confirmar la reserva, se debe abonar una seña del 50% del
-                  valor total de la estadía. La seña no es reembolsable. El
-                  saldo restante deberá abonarse AL MOMENTO DE LLEGAR AL
-                  ALOJAMIENTO EN EFECTIVO (PESOS O DOLARES).
-                </li>{" "}
-                <li>LA CASA TIENE UNA capacidad máxima de 6 personas.</li>
-                <li>El check-in es a las 14:00 y el check-out a las 11:00</li>
-              </ul>
-            </h3>
+            <h3>ACLARACIONES: </h3> <br />
+            <ul>
+              <li>
+                Fines de semana largos se alquilan completos sin excepción.
+              </li>
+              <li> Las tarifas pueden ser modificadas sin previo aviso.</li>
+              <li>
+                Para confirmar la reserva, se debe abonar una seña del 50% del
+                valor total de la estadía. La seña no es reembolsable. El saldo
+                restante deberá abonarse AL MOMENTO DE LLEGAR AL ALOJAMIENTO EN
+                EFECTIVO (PESOS O DOLARES).
+              </li>{" "}
+              <li>LA CASA TIENE UNA capacidad máxima de 6 personas.</li>
+              <li>El check-in es a las 14:00 y el check-out a las 11:00</li>
+            </ul>
           </Grid>
         </Grid>
       </Box>
